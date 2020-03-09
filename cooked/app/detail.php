f2f6d3c08a1ec1ff6d46d5c1e87d2b4d132644e5
CAVOINHO <kentnguyen101@gmail.com>
2020-03-09 09:16:58 +0700

    NLCS

---
+++ cooked/app/detail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class detail extends Model
+{
+    protected $table = "detail";
+
+    public function product() {
+    	return $this->belongsTo('App\product', 'idproduct', 'id');
+    }
+}
---
+++ cooked/resources/views/admin/detail/list.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.layout.index')
+
+@section('content');
+<!-- Page Content -->
+<div id="page-wrapper">
+    @if(session('thongbao'))
+        <div class="alert alert-success">
+            {{session('thongbao')}}
+        </div>
+    @endif
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header"> Chi tiết
+                    <small>Danh sách</small>
+                </h1>
+            </div>
+
+            <!-- /.col-lg-12 -->
+            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                <thead>
+                    <tr align="center">
+                        <th class="text-center">ID</th>
+                        <th class="text-center">ID Món ăn</th>
+                        <th class="text-center">Bước</th>
+                        <th class="text-center">Nội dung</th>
+                        <th class="text-center">Nội dung không dấu</th>
+                        <th class="text-center">Hình ảnh 1</th>
+                        <th class="text-center">Hình ảnh 2</th>
+                        <th class="text-center">Hình ảnh 3</th>
+                        <th class="text-center">Hình ảnh 4</th>
+                        <th class="text-center">Hình ảnh 5</th>
+                        <th class="text-center">Hình ảnh 6</th>
+                        <th class="text-center">Xóa</th>
+                        <th class="text-center">Sửa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   @foreach($detail as $de)
+                        <tr class="odd gradeX" align="center">
+                            <td>{{ $de->id }}</td>
+                            <td>{{ $de->idproduct }}</td>
+                            <td>{{ $de->step }}</td>
+                            <td>{{ $de->content }}</td>
+                            <td>{{ $de->contentkd }}</td>
+                            <td>{{ $de->img1 }}</td>
+                            <td>{{ $de->img2 }}</td>
+                            <td>{{ $de->img3 }}</td>
+                            <td>{{ $de->img4 }}</td>
+                            <td>{{ $de->img5 }}</td>
+                            <td>{{ $de->img6 }}</td>
+                            <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="admin/detail/delete/{{ $de->id }}"> Xóa</a></td>
+                            <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="admin/detail/edit/{{ $de->id }}">Sửa</a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>    
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+@endsection
